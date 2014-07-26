cf7da2575d3d818aa6c267264cb9b17040cefc72
dkl <dkl@akiba.com>
2014-07-25 19:34:59 -0700

    exercises in ch 7

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -29,8 +29,15 @@ describe "User pages" do
             it "should not create a user" do
                 expect { click_button submit }.not_to change(User, :count)
             end
-        end
 
+            describe "after submission" do
+                before { click_button submit }
+
+                it { should have_title('Sign Up') }
+                it { should have_content('error') }
+            end
+        end
+        
         describe "with valid info" do
             before do
                 fill_in "Name", with: "Example User"
@@ -38,10 +45,18 @@ describe "User pages" do
                 fill_in "Password", with: "foobar"
                 fill_in "Confirmation", with: "foobar"
             end
-
+            
             it "should create a user" do
                 expect { click_button submit }.to change(User, :count).by(1)
             end
+
+            describe "after saving the user" do
+                before { click_button submit }
+                let(:user) { User.find_by(email: 'user@example.com') }
+                it { should have_title(user.name) }
+                it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+            end
+
         end
     end
 end
